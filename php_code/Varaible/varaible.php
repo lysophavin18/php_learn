8a6ef5db4afa0fc863ccfa252245592b056f068a
sophavin <ly.sophavin1122004@gmail.com>
2023-09-21 15:58:45 +0700

    learning php

---
+++ php_code/Varaible/varaible.php
@@ -0,0 +1,17 @@
+<?php
+$age = 34; //declare varailble in php like iterger
+$name = " sophavin "; //declare varailble in php Srting 
+$class = 'C '; // it string same 
+$number1 = 1; //declare varaible 
+$number2 = 1; //same
+
+
+echo $age; // print out put 
+echo $name;
+echo $class;
+//math php .
+echo $number1 + $number2; // calculate like math +
+echo $number1 / $number2; //   
+echo $number1 - $number2;
+echo $number1 * $number2;
+echo $number1 % $number2;
---
+++ php_code/print/hello.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+    <title>Hello World Script</title>
+</head>
+
+<body>
+    <p>Hello guy</p>
+</body>
+
+</html>
