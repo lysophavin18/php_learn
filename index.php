6f0681b403053aaeb0e3c86f0f101f3b0c857f30
sophavin <ly.sophavin1122004@gmail.com>
2023-09-23 23:23:51 +0700

    Changes

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+     <style>
+     body{
+             place-items: center;
+             display: grid;
+             height: 100vh;
+             margin: 0;
+             font-family: sans-serif;
+     }
+     </style>
+<body>
+ <h1>
+     <?php
+     echo "You Won 10$";
+      ?>
+ </h1>
+
+</body>
+</html>
