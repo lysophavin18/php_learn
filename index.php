a6e4cd32f85f774614ff100882229bf3695c4777
sophavinly18 <https://github.com/lysophavin18/ProjectPratisCumY3>
2024-01-06 15:30:12 +0700

    add Welcome.txt

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>mywebsite</title>
 </head>
-     <style>
-     body{
-             place-items: center;
-             display: grid;
-             height: 100vh;
-             margin: 0;
-             font-family: sans-serif;
-     }
-     </style>
+<style>
+    body {
+        place-items: center;
+        display: grid;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
+</style>
+
 <body>
- <h1>
-     <?php
-     echo "You Won 10$";
-      ?>
- </h1>
+    <h1>
+        <?php
+        $color = "red";
+        echo "You Won 10$";
+        ?>
+    </h1>
 
 </body>
+
 </html>
